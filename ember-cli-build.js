9ddff1fe5ff555f8016147bac4f302cb3998a665
Daniil Chapovskyi <encore.ipost@gmail.com>
2023-09-13 14:19:33 -0700

    update

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -14,5 +14,7 @@ module.exports = function(defaults) {
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
 
+  app.import('node_modules/uri-templates/uri-templates.js');
+
   return app.toTree();
 };
